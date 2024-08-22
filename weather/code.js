2974c19f38d464c5a527be2e88f2fc40e5229759
cristianwolff33 <cristianwolff@proton.me>
2024-08-22 20:57:14 +0200

    geolocalization function

--- weather/code.js
+++ weather/code.js
@@ -1,16 +1,19 @@
 let lat;
 let long;
-
-
-
+let apiKey = "f03964241c61053875ad71c776cdb4f6 ";
 
 function startApp (){
-    console.log("Hello");
 
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                lat = position.coords.latitude;
+                long = position.coords.longitude;
+                console.log("lat:", lat, "long:", long);
 
-    if(navigator.geolocation.){
-        
+                
+            }
+        );
     }
 }
-
 
