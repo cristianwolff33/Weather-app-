2e7dfdaed77ab86740f84d0cd0ad113225a016d7
cristianwolff33 <cristianwolff@proton.me>
2024-08-28 22:41:43 +0200

    zmiany 8.08.2024

--- weather/code.js
+++ weather/code.js
@@ -1,26 +1,55 @@
 let lat;
 let long;
-let apiKey = "f03964241c61053875ad71c776cdb4f6 ";
+const apiKey = "9d6c1963fdb9a8f890aaf3a31b9e699d";
 
-function startApp (){
+function startApp() {
 
-    if (navigator.geolocation){
+    if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
             (position) => {
                 lat = position.coords.latitude;
                 long = position.coords.longitude;
+                
                 console.log("lat:", lat, "long:", long);
 
                 getWeatherData();
             }
         );
     }
-};
+}
 
-
-
-function getWeatherData(){
+function getWeatherData() {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
     console.log(url);
 
+    fetch(url).then( function(response){
+        response.json().then( function(data){
+            console.log(data);
+            updateWeatherData(data);
+        
+        } );
+    });
 };
+
+function updateWeatherData(data){
+    const temp = data.main.temp;
+    const local = data.name;
+    const humidity = data.main.humidity;
+    const pressure = data.main.pressure;
+    const cloudy = data.weather[0].description;
+    const windSpeed = data.wind.speed;
+    const sunRise = new Date(data.sys.sunrise * 1000);
+    const sunSet = new Date(data.sys.sunset * 1000);
+
+
+
+    document.getElementById("temp").innerHTML = temp;
+    document.getElementById("local").innerHTML = local;
+    document.getElementById("humidity").innerHTML = humidity;
+    document.getElementById("pressure").innerHTML = pressure;
+    document.getElementById("cloudsPerc").innerHTML = cloudy;
+    document.getElementById("windSpeed").innerHTML = windSpeed;
+    document.getElementById("sunRise").innerHTML = sunRise;
+    document.getElementById("sunSet").innerHTML = sunSet;
+};
+
